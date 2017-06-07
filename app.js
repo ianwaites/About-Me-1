e78be95e9607e178262883a6e4746584cabc1624
Cameron <cameron.32090@gmail.com>
2017-06-07 08:16:29 -0700

    About Me Page

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/*var question1 = prompt('Is Cameron from the United States? Y or N').toUpperCase();
+if (question1 === 'Y') {
+  alert('Yes Cameron is from Texas!')
+} else {
+  alert('Nice try, Cameron is actully from Texas.')
+};
+console.log('Cameron is form the United States')
+var question2 = prompt('Did Cameron really move 12 times as a child? Y or N').toUpperCase();
+if (question2 === 'Y') {
+  alert('Cameron only moved 6 times, not 12.')
+} else {
+  alert('Correct! Cameron moved 6 times as a child.')
+};
+console.log('Cameron moved 6 times')
+var question3 = prompt('Is Cameron a vegan? Y or N').toUpperCase();
+if (question3 === 'Y') {
+  alert('Yes Cameron is vegan, but will cheat from time to time.')
+} else {
+  alert('Cameron is vegan, but will cheat from time to time.')
+};
+console.log('Cameron is a cheating vegan')
+var question4 = prompt('Does Cameron need his coffee in the morning? Y or N').toUpperCase();
+if (question4 === 'Y') {
+  alert('Unlike the rest of the Northwest, Cameron drinks green tea in the morning.')
+} else {
+  alert('Right! Cameron does not drink coffee.')
+};
+console.log('Cameron does not drink coffee.')
+var question5 = prompt('Is Bill Gates one of Camerons inspirations? Y or N').toUpperCase();
+if (question5 === 'Y') {
+  alert('Cam has much respect, but Bill Gates is not his inspiration.')
+} else {
+  alert('Right! Cameron is more of a Steve Jobs fan.')
+};
+console.log('Cameron is a Steve Jobs fan')
